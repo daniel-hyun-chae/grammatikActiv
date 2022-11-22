821c7c6d75df636b012289b7a83713d939b704e5
daniel-hyun-chae <daniel.hyun.chae@gmail.com>
2022-11-22 18:05:15 +0100

    Implemented the basis for dark theme

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,19 +3,32 @@ import {
   UserCircleIcon,
   CursorArrowRippleIcon,
 } from "@heroicons/react/24/solid";
-import type { ReactNode } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import type { Dispatch, ReactNode, SetStateAction } from "react";
 import SidebarLink from "./SidebarLink";
 import TopNavLink from "./TopNavLink";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({
+  children,
+  darkMode,
+  setDarkMode,
+}: {
+  darkMode: boolean;
+  children: ReactNode;
+  setDarkMode: Dispatch<SetStateAction<boolean>>;
+}) {
+  function toggleDarkMode() {
+    localStorage.setItem("darkMode", String(!darkMode));
+    setDarkMode(!darkMode);
+  }
   return (
-    <div className="APP flex h-screen w-screen flex-col">
-      <div className="TOP-BAR flex w-screen items-center justify-between py-3 px-5">
+    <div className="[APP] flex h-screen w-screen flex-col overflow-hidden text-sm dark:bg-gray-900 dark:text-gray-200">
+      <div className="[TOP-BAR] sticky flex w-screen items-center justify-between p-1 ">
         <div className="TOP-BAR-LEFT flex	basis-4/12 items-center space-x-5">
-          <Bars3Icon className="h-7 w-7" />
+          <Bars3Icon className="hidden h-7 w-7 lg:block" />
           <div className="flex items-center space-x-2">
-            <CursorArrowRippleIcon className="h-6 w-6 text-violet-400" />
-            <span className="font-titilium text-lg font-semibold">
+            <CursorArrowRippleIcon className="h-12 w-12 text-violet-400" />
+            <span className="text-md font-titilium font-semibold  lg:text-lg">
               Interactive Learning
             </span>
           </div>
@@ -32,17 +45,28 @@ export default function Layout({ children }: { children: ReactNode }) {
             <TopNavLink href="/courses" text="Courses" />
             <TopNavLink href="/publisher" text="Publisher" />
           </nav>
-          <UserCircleIcon className="h-8 w-8" />
+          <div onClick={toggleDarkMode} className="hover:cursor-pointer">
+            {darkMode ? (
+              <SunIcon className="h-7 w-7" />
+            ) : (
+              <MoonIcon className="h-7 w-7" />
+            )}
+          </div>
+          <UserCircleIcon className="h-7 w-7" />
         </div>
       </div>
-      <div className="BELOW-TOP-BAR flex h-full pt-3">
-        <div className="SIDE-BAR flex flex-col px-5">
-          <nav className="SIDE-BAR-NAV flex flex-col">
+      <div className="[BELOW-TOP-BAR] flex h-18/20 w-full overflow-y-auto pt-3">
+        <div className="[SIDE-BAR] hidden lg:visible lg:flex lg:flex-col lg:px-5">
+          <nav className="[SIDE-BAR-NAV] flex flex-col">
             <SidebarLink href="/publisher" text="Publisher Home" />
           </nav>
         </div>
-        <main className="MAIN flex-grow">{children}</main>
+        <main className="[MAIN] flex-grow overflow-y-auto">{children}</main>
       </div>
+      <nav className="[BOTTOM-BAR] flex h-1/20 w-full justify-center bg-gray-100 lg:hidden">
+        <TopNavLink href="/publisher" text="Publisher Home" />
+        <TopNavLink href="/publisher" text="Publisher" />
+      </nav>
     </div>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,41 @@ import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { useEffect, useState } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  useEffect(() => {
+    themeCheck();
+  }, []);
+
+  function themeCheck() {
+    if (
+      localStorage.getItem("darkMode") === "true" ||
+      (!("darkMode" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      setDarkMode(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setDarkMode(false);
+    }
+  }
   return (
     <SessionProvider session={session}>
-      <Layout>
+      <Layout darkMode={darkMode} setDarkMode={setDarkMode}>
         <Component {...pageProps} />
       </Layout>
     </SessionProvider>
